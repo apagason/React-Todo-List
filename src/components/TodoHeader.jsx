b0b568f7ceb0afafedf333c44b256f65c61d4849
apagason <109061082+apagason@users.noreply.github.com>
2022-08-17 22:45:10 +0800

    initial commit

---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+
+function TodoHeader({ addTodo }) {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTodo({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="new-task-form">
+      <div>
+        <input
+          id="new-task-input"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          name="text"
+          className="todo-input"
+          placeholder="What do you have planned?"
+          style={{ with: "600px" }}
+        />
+
+        <button id="new-task-submit">Add Todo</button>
+      </div>
+    </form>
+  );
+}
+
+export default TodoHeader;
